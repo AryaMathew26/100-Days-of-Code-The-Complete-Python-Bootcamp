c7ea401bb9d4635a0f5ae32f6ef5aed5dc8e9d4c
AryaMathew26 <104437264+AryaMathew26@users.noreply.github.com>
2022-09-13 11:56:25 +0530

    Calculator

---
+++ Day_10.py
@@ -0,0 +1,49 @@
+import replit 
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+def add(n1,n2):
+  return n1+n2
+def subtract(n1,n2):
+  return n1-n2
+def multiply(n1,n2):
+  return n1*n2
+def divide(n1,n2):
+  return n1/n2
+operations={
+            '+':add,
+            '-':subtract,
+            '*':multiply,
+            '/':divide
+            }
+
+def calculator():
+  print(logo)
+  should_continue=True
+  first_num=float(input('What is the first number?'))
+  while should_continue:
+    operator=input('Pick an operation: +, -, * or /.')
+    second_num=float(input('What is the second number?'))
+    calc_function=operations[operator]
+    result=calc_function(first_num,second_num)
+    print(f'{first_num}{operator}{second_num}={result}')
+    if input("Type 'yes' if you want to continue with the previous answer. Type 'no' to begin a new calculation.")=='yes':
+      first_num=result
+    else:
+      should_continue=False
+      replit.clear()
+      calculator()
+calculator()      
